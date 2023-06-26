7fca4b1674152bc8a9b6c6a13a4fbf8f4d98a0a8
Jake Alagel <alagel.jacob@gmail.com>
2023-06-25 22:31:21 -0400

    summoner name and champion mastery by id api calls added

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,51 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
+const cors = require('cors');
+const axios = require('axios');
+
+
+dotenv.config();
+
+app.use(cors());
+
+app.get('/api', (req: any, res: any) => {
+    res.send('Hello from the API');
+});
+
+app.get('/summoner/:name', async (req: any, res: any) => {
+    const { name } = req.params;
+    const riotApiKey = process.env.RIOT_API_KEY;
+    console.log(riotApiKey);
+
+    try {
+        const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${riotApiKey}`);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error getting data from Riot API');
+    }
+});
+
+app.get('/champion-mastery/:id', async (req: any, res: any) => {
+    const { id } = req.params;
+    const riotApiKey = process.env.RIOT_API_KEY;
+
+    try {
+        const response = await axios.get(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}?api_key=${riotApiKey}`);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving data from Riot API');
+    }
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+console.log(process.env.RIOT_API_KEY)
+
+
+
+
