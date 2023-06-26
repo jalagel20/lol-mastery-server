a56a406b493c29b7c695c9ec4a3c17b1154b2c2e
Jake Alagel <alagel.jacob@gmail.com>
2023-06-25 22:35:50 -0400

    prettier config

--- server/index.ts
+++ server/index.ts
@@ -5,47 +5,42 @@ const port = process.env.PORT || 5001;
 const cors = require('cors');
 const axios = require('axios');
 
-
 dotenv.config();
 
 app.use(cors());
 
 app.get('/api', (req: any, res: any) => {
-    res.send('Hello from the API');
+  res.send('Hello from the API');
 });
 
 app.get('/summoner/:name', async (req: any, res: any) => {
-    const { name } = req.params;
-    const riotApiKey = process.env.RIOT_API_KEY;
-    console.log(riotApiKey);
+  const { name } = req.params;
+  const riotApiKey = process.env.RIOT_API_KEY;
 
-    try {
-        const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${riotApiKey}`);
-        res.send(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error getting data from Riot API');
-    }
+  try {
+    const response = await axios.get(
+      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${riotApiKey}`,
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error getting data from Riot API');
+  }
 });
 
 app.get('/champion-mastery/:id', async (req: any, res: any) => {
-    const { id } = req.params;
-    const riotApiKey = process.env.RIOT_API_KEY;
+  const { id } = req.params;
+  const riotApiKey = process.env.RIOT_API_KEY;
 
-    try {
-        const response = await axios.get(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}?api_key=${riotApiKey}`);
-        res.send(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error retrieving data from Riot API');
-    }
+  try {
+    const response = await axios.get(
+      `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}?api_key=${riotApiKey}`,
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving data from Riot API');
+  }
 });
 
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-console.log(process.env.RIOT_API_KEY)
-
-
-
-
